bd4bb559971856517775ca27fbe57d809e664220
Jugal Kishore <me@devjugal.com>
2025-07-04 10:53:55 +0530

    Initial Commit - Project up!
    .gitignore
    .python-version
    README.md
    main.py
    pyproject.toml
    uv.lock

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This is the main file for the project.
+"""
+
+__author__ = "Jugal Kishore <me@devjugal.com>"
+
+
+def main():
+    print("Hello from ipo-monitor!")
+
+
+if __name__ == "__main__":
+    main()
